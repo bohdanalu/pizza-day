21f72a2506e73fb8d1f2416aef5c9c705f12ad80
bohdanalu <b_dana@ukr.net>
2024-02-15 20:59:40 +0200

    move the request to Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,35 @@
 import MenuList from "./MenuList/MenuList";
+import { useEffect, useState } from "react";
+import { API_URL } from "../../helpers/constants";
 import styles from "./Menu.module.css";
 
 const Menu = () => {
+  const [menuList, setMenuList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getDate = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+          throw new Error("Failed to fetch menu");
+        }
+        const { data } = await response.json();
+        setMenuList(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getDate();
+  }, []);
   return (
     <section className={styles.menu__section}>
       <h2 className={styles.menu__title}>Menu</h2>
-      <MenuList />
+      <MenuList menuList={menuList} loading={loading} />
+      {error && <div>Error: {error}</div>}
     </section>
   );
 };
--- src/components/Menu/MenuList/MenuList.jsx
+++ src/components/Menu/MenuList/MenuList.jsx
@@ -1,37 +1,9 @@
-import { useEffect, useState } from "react";
 import MenuItem from "../MenuItem/MenuItem";
-import { API_URL } from "../../../helpers/constants";
 import styles from "./MenuList.module.css";
 
-const MenuList = () => {
-  const [menuList, setMenuList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getDate = async () => {
-      try {
-        const response = await fetch(API_URL);
-        if (!response.ok) {
-          throw new Error("Failed to fetch menu");
-        }
-        const { data } = await response.json();
-        setMenuList(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getDate();
-  }, []);
-
+const MenuList = ({ menuList, loading }) => {
   if (loading) {
     return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
   }
 
   if (!menuList) {
