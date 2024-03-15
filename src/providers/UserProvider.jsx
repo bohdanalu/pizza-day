2572adcac7fdd6162012080179829b26263d6660
bohdanalu <b_dana@ukr.net>
2024-03-15 14:03:54 +0200

    Update user provider

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,10 +1,21 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 export const UserContext = createContext(null);
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState("");
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+    localStorage.setItem("user", newUser);
+  };
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, updateUser }}>
       {children}
     </UserContext.Provider>
   );
